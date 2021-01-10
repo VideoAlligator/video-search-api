5b92fc997e0bc261979e8e79f6a010accbb25399
yixuannliu <yixuann.liu.utoronto@gmail.com>
2021-01-10 19:57:51 +0800

    validate body

--- src/routes/videos.ts
+++ src/routes/videos.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express'
+import Joi from '@hapi/joi'
+
 import VideoController from '../controllers/videos'
+import { validateBody } from '../middleware/validate'
 
 const router = Router()
 
-router.post('/videos', async (req, res) => {
+const VIDEO_DETAIL = Joi.object().keys({
+  keyword: Joi.string().required(),
+  timestamp: Joi.string().required(),
+})
+
+const VIDEO_SCHEMA = Joi.object({
+  title: Joi.string().required(),
+  duration: Joi.number().required(),
+  overview: Joi.string(),
+  genres: Joi.array().items(Joi.string()),
+  keywords: Joi.array().items(Joi.string()),
+  posterUrl: Joi.string().required(),
+  details: Joi.array().items(VIDEO_DETAIL),
+})
+
+router.post('/videos', validateBody(VIDEO_SCHEMA), async (req, res) => {
   VideoController.create(res, req.body)
 })
 
@@ -11,7 +29,7 @@ router.get('/videos', async (req, res) => {
   VideoController.list(res)
 })
 
-router.get('/videos/:videoId', async (req, res) => {
+router.get('/videos/:videoId', validateBody(VIDEO_SCHEMA), async (req, res) => {
   VideoController.retrieve(res, req.params.videoId)
 })
 
