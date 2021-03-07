2ad7ca1a6e71170c405113bdf20aa1d7e16ce1e2
yixuannliu <yixuann.liu.utoronto@gmail.com>
2021-03-07 18:45:03 +0800

    fix:dotenv

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import cors from 'cors'
 import routes from './routes'
 import connect from '../src/connect'
 
+require('dotenv').config()
+
 const app: Application = express()
-const port = 8080
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -16,6 +17,7 @@ app.get('/', (req: Request, res: Response) =>
   res.send('Welcome to the Mongoose & TypeScript example')
 )
 
+const port = process.env.PORT || 8000
 app.listen(port, () =>
   console.log(`Application started successfully on port ${port}.`)
 )
