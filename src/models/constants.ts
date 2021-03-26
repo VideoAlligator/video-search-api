cb80ec95dc4ddb0825b20dafde86a6659cac1076
yixuannliu <yixuann.liu.utoronto@gmail.com>
2021-03-26 22:31:26 +0800

    update

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -9,16 +9,22 @@ export enum Keyword {
   BUILDINGS = 'buildings',
   STREET = 'street',
   MOUNTAIN = 'mountain',
+  DINING_TABLE = 'dining table',
+  KITE = 'kite',
+  BEAR = 'bear',
 }
 
 export enum Genres {
+  ACTION = 'Action',
   ADVENTURE = 'Adventure',
   ANIMATION = 'Animation',
   COMEDY = 'Comedy',
   DRAMA = 'Drama',
   FAMILY = 'Family',
   FANTASY = 'Fantasy',
+  FOREIGN = 'Foreign',
   ROMANCE = 'Romance',
   SCIENCE_FICTION = 'Science Fiction',
   THRILLER = 'Thriller',
+  TV_MOVIE = 'TV Movie',
 }
